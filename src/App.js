1d515daa812fb12f7831bd477aaf8725b34f99df
Niels Dominguez <niels.dominguez@gmail.com>
2019-06-04 10:22:42 +0700

    clean

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-import Header from './Header'
-import Menu from './Menu'
-import List from './List'
 import Triangle from './Triangle'
 
 
